9f957d841b3b915b96e471c3dcf3f45ee2388e82
Mattpott <mattpotter562@gmail.com>
2022-10-10 13:25:00 -0400

    Implemented Dynamic Rows

--- js/ch08-proj01.js
+++ js/ch08-proj01.js
@@ -4,3 +4,10 @@ const shipping_threshold = prompt('Enter shipping threshold (1000)');
 
 /* add loop and other code here ... in this simple exercise we are not
    going to concern ourselves with minimizing globals, etc */
+
+function outputTableBody() {
+   for (let item of cart) {
+      let total = calculateTotal(item.quantity, item.product.price);
+      outputCartRow(item, total);
+   }
+}
--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,19 @@
 /* define your functions here */
+function calculateTotal(quantity, price) {
+    return quantity * price;
+}
+
+function outputCartRow(item, total) {
+    const row = `<tr>
+    <td><img src="images/${item.product.filename}"></td>
+    <td>${item.product.title}</td>
+    <td>${item.quantity}</td>
+    <td>${item.product.price}</td>
+    <td>${total}</td>
+    </tr>`
+    document.write(row)
+}
 
 
 
 
-
-
-
-
-        
